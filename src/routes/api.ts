b8c2b65f4e5badd36727a0427978b12763f01f57
Kris Magat <kmag.dev@gmail.com>
2023-03-10 22:37:16 -0800

    added comments

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
-
 const router = express.Router();
 
+//write your middleware here or in a different file. 
+//this is your middleware structure
 const middleware = (req: Request, res: Response, next: any) => {
   try {
     res.send('Success!');
@@ -11,12 +12,10 @@ const middleware = (req: Request, res: Response, next: any) => {
   }
 };
 
-router.get('/', middleware);
-
-router.post('/', middleware);
-
-router.put('/', middleware);
-
-router.delete('/', middleware);
+//you can create separate routers to serve different endpoints and different API's
+router.get('/', middleware, (req, res) => res.status(200).send('Success!'));
+router.post('/', middleware, (req, res) => res.status(200).send('Success!'));
+router.put('/', middleware, (req, res) => res.status(200).send('Success!'));
+router.delete('/', middleware, (req, res) => res.status(200).send('Success!'));
 
 export { router as apiRouter };
